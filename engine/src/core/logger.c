3ec4f80c481d660d82ba47532a80c85addb10a79
Ryan Caldwell <stunthound@gmail.com>
2021-08-22 16:00:05 -0400

    Windows platform

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,27 +1,32 @@
 #include "logger.h"
 #include "asserts.h"
+#include "platform/platform.h"
 
 // TODO: temporary
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
-b8 initialize_logging() {
+b8 initialize_logging() 
+{
     // TODO: create log file.
     return TRUE;
 }
 
-void shutdown_logging() {
+void shutdown_logging() 
+{
     // TODO: cleanup logging/write queued entries.
 }
 
-void log_output(log_level level, const char* message, ...) {
+void log_output(log_level level, const char* message, ...) 
+{
     const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]:  ", "[INFO]:  ", "[DEBUG]: ", "[TRACE]: "};
-    // b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARN;
 
     // Technically imposes a 32k character limit on a single log entry, but...
     // DON'T DO THAT!
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     // Format original message.
@@ -30,16 +35,21 @@ void log_output(log_level level, const char* message, ...) {
     // which is the type GCC/Clang's va_start expects.
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, 32000, message, arg_ptr);
+    vsnprintf(out_message, msg_length, message, arg_ptr);
     va_end(arg_ptr);
 
-    char out_message2[32000];
+    char out_message2[msg_length];
     sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
 
-    // TODO: platform-specific output.
-    printf("%s", out_message2);
+    // Platform-specific output.
+    if (is_error) {
+        platform_console_write_error(out_message2, level);
+    } else {
+        platform_console_write(out_message2, level);
+    }
 }
 
-void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) {
+void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) 
+{
     log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, message: '%s', in file: %s, line: %d\n", expression, message, file, line);
 }
