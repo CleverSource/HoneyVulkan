3931810dd271fa67dee52f9b7298f5193537797f
Ryan Caldwell <stunthound@gmail.com>
2022-04-09 17:09:38 -0400

    Implemented vulkan extensions, validation layers, and debug messenger

--- engine/src/core/hstring.h
+++ engine/src/core/hstring.h
@@ -6,3 +6,6 @@
 HAPI u64 string_length(const char* str);
 
 HAPI char* string_duplicate(const char* str);
+
+// Case-sensitive string comparison. True if the same, otherwise false.
+HAPI b8 strings_equal(const char* str0, const char* str1);
